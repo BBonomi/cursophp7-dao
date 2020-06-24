f68d448400ab6073276c1426ead5cca8f4470d23
BBonomi <bbaratari@gmail.com>
2020-06-24 14:03:10 -0300

    Metodos de Lista

--- class/Usuario.php
+++ class/Usuario.php
@@ -46,6 +46,36 @@ class Usuario {
 			$this->setDtcadastro ( new DateTime ( $row ['dtcadastro'] ) ); // new DateTime coloca no padrão data e hora
 		}
 	}
+
+	// Metodo List Aula 65
+	public static function getList() {
+		$sql = new Sql ();
+		return $sql->select ( "SELECT * FROM tb_usuarios ORDER BY deslogin;" );
+	}
+	public static function search($login) {
+		$sql = new Sql ();
+		return $sql->select ( "SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array (
+				':SEARCH' => "%" . $login . "%"
+		) );
+	}
+	public function login($login, $password) {
+		$sql = new Sql ();
+		$results = $sql->select ( "SELECT * FROM tb_usuarios WHERE deslogin = : LOGIN AND dessenha = :PASSWORD", array (
+				":LOGIN" => $login,
+				":PASSWORD" => $password
+		) );
+		// if(isset($results[0])) //verifica se existe(isset) no results no indice 0
+		if (count ( $results ) > 0) {
+			$row = $results [0];
+
+			$this->setIdusuario ( $row ['idusuario'] );
+			$this->setDeslogin ( $row ['deslogin'] );
+			$this->setDesenha ( $row ['dessenha'] );
+			$this->setDtcadastro ( new DateTime ( $row ['dtcadastro'] ) ); // new DateTime coloca no padrão data e hora
+		} else {
+			throw new Exception ( "Login e/ou senha inválidos." );
+		}
+	}
 	public function __toString() {
 		return json_encode ( array (
 				"idusuario" => $this->getIdusuario (),
--- index.php
+++ index.php
@@ -11,8 +11,24 @@ require_once ("config.php");
 
 // Aula 64 PDO - DAO - SELECT
 
-$root = new Usuario (); // chamando classe Usuario
-$root->loadById ( 3 ); // chamando o metodo loadByID usuario 3 do banco de dados
-echo $root;
+/*
+ * Carrega um usuario
+ * $root = new Usuario (); // chamando classe Usuario
+ * $root->loadById ( 3 ); // chamando o metodo loadByID usuario 3 do banco de dados
+ * echo $root;
+ */
+
+// Carrega uma lista de usuarios aula 65
+// $lista = Usuario::getList ();
+// echo json_encode ( $lista );
+
+// Carrega uma lista de usuarios buscando pelo login aula 65
+// $search = Usuario::search ( "jo" );
+// echo json_encode ( $search );
+
+// Carregando um usuario usando login e senha
+$usuario = new Usuario ();
+$usuario->login ( "joao", "32165" );
+echo $usuario;
 
 ?>
